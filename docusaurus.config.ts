6ce2196ae1856813cdbcf77b2abf5462f625ff7a
Emran025 <amran.naser@ibbuniv.edu.ye>
2026-04-15 16:46:39 +0300

    chore: update base URL for standalone hosting (Vercel/Netlify)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,8 +17,7 @@ const config: Config = {
   // Set the production url of your site here
   url: 'https://Emran025.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/qayd-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
